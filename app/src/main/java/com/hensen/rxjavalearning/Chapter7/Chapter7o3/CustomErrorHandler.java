0773534c3b06d52934b02a696cb4c2ad554763d1
吴焕楠 <wuhuannan@meizu.com>
2020-03-28 22:53:28 +0800

    整理代码

--- app/src/main/java/com/hensen/rxjavalearning/Chapter7/Chapter7o3/CustomErrorHandler.java
+++ app/src/main/java/com/hensen/rxjavalearning/Chapter7/Chapter7o3/CustomErrorHandler.java
@@ -3,7 +3,8 @@ package com.hensen.rxjavalearning.Chapter7.Chapter7o3;
 import android.util.Log;
 
 import io.reactivex.functions.Consumer;
-import rx.plugins.RxJavaErrorHandler;
+import me.jessyan.rxerrorhandler.core.RxErrorHandler;
+//import rx.plugins.RxJavaErrorHandler;
 
 /**
  * Created by handsomexu on 2018/10/6.
